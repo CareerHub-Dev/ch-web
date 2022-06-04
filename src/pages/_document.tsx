65ef847d4a5b53c87766fd2ae9c0419dcbf7870e
Sergei Burtsev <spburtsev@gmail.com>
2022-06-04 22:03:16 +0300

    Done some customization on Document component and Home page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,13 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document';
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: any) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
@@ -10,19 +16,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
           <link
             href="https://fonts.googleapis.com/icon?family=Material+Icons"
             rel="stylesheet"
           />
-          <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
           <link
             rel="stylesheet"
             href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
             integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
             crossOrigin="anonymous"
           />
-          <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
         </Head>
         <body>
           <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,29 @@
-import type { NextPage } from 'next';
 import NureLogo from '@/assets/logos/NureLogo.svg';
 import CareerLogo from '@/assets/logos/CareerLogo.svg';
+import Head from 'next/head';
 
 import classes from '@/styles/index.module.scss';
 
-const LandingPage: NextPage = () => {
+const LandingPage = () => {
   return (
-    <div className={classes.root}>
-      <div className={classes['nure-logo']} id="partnerLogos">
-        <NureLogo id="nureLogo" />
-        <CareerLogo id="careerLogo" />
+    <>
+      <Head>
+        <title>{'CareerHub 🇺🇦'}</title>
+        <meta
+          name="description"
+          content={`CareerHub - це сервіс пошуку вакансій для студентів ХНУРЕ від студентів ХНУРЕ. Розроблено при підтримці центра 'Кар'єра'.`}
+        />
+      </Head>
+      <div className={classes.root}>
+        <div className={classes['nure-logo']} id="partnerLogos">
+          <NureLogo id="nureLogo" />
+          <CareerLogo id="careerLogo" />
+        </div>
+        <div className={classes.content}>
+          <h1 className={classes.title}>CareerHub</h1>
+        </div>
       </div>
-      <div className={classes.content}>
-        <h1 className={classes.title}>CareerHub</h1>
-      </div>
-    </div>
+    </>
   );
 };
 
