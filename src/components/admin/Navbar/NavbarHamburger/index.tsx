8ef05208533da652c58e71ca0bf9acc04751ff99
Yevhenii <yevhenii.bakhmat@nure.ua>
2022-07-23 10:02:14 +0300

    Made hamburger icon color based on theme prop

--- src/components/admin/Navbar/NavbarHamburger/index.tsx
+++ src/components/admin/Navbar/NavbarHamburger/index.tsx
@@ -3,18 +3,31 @@ import classes from './style.module.scss';
 export type Props = {
   isOpen: boolean;
   onClick?: (isOpen: boolean) => void;
+  theme?: 'dark' | 'light';
 };
 
-const NavbarHamburger: FC<Props> = ({ isOpen = true, onClick }) => {
+const NavbarHamburger: FC<Props> = ({
+  isOpen = true,
+  onClick,
+  theme = 'dark',
+}) => {
   const handleClick = () => {
     if (onClick) {
       onClick(!isOpen);
     }
   };
+  const themeStyles: { [key: string]: string } = {
+    dark: classes.dark,
+    light: classes.light,
+  };
 
   return (
     <button className={classes.iconWrapper} onClick={handleClick}>
-      <div className={`${classes.hamburger} ${isOpen && classes.isActive}`} />
+      <div
+        className={`${classes.hamburger} ${isOpen && classes.isActive} ${
+          themeStyles[theme]
+        }`}
+      />
     </button>
   );
 };
