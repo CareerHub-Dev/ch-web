24b04d003c73de8db384ef694055aa83365d483d
Sergei Burtsev <spburtsev@proton.me>
2022-10-03 00:19:18 +0300

    temporarily removed axios from api index

--- src/lib/api/remote/index.ts
+++ src/lib/api/remote/index.ts
@@ -1,6 +1,5 @@
-type Headers = {
-  [key: string]: string;
-};
+export const serverUrl = process.env.BACKEND_SERVER_URL;
+export const baseURL = `${serverUrl}/api/`;
 
 export const retrieveErrorMessage = (responseData: any) => {
   if (responseData.message) {
@@ -16,43 +15,4 @@ export const retrieveErrorMessage = (responseData: any) => {
     return responseData.title;
   }
   return 'Невідома помилка';
-};
-
-export const serverUrl = process.env.BACKEND_SERVER_URL;
-export const baseURL = `${serverUrl}/api/`;
-export const defaultTimeout = 10000;
-export const defaultTimeoutErrorMessage = 'Сервер мовчить';
-export const defaultHeaders = {
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*',
-};
-export const defaultAxiosConfig = {
-  baseURL,
-  timeout: defaultTimeout,
-  timeoutErrorMessage: defaultTimeoutErrorMessage,
-  headers: defaultHeaders,
-};
-
-export const getAccessTokenFromLocalStorage = () => {
-  if (typeof localStorage === 'undefined') {
-    throw new Error('LocalStorage is not supported');
-  }
-
-  const accessToken = localStorage.getItem('accessToken');
-  const accessTokenIsValid = accessToken && accessToken.length > 0;
-  if (accessTokenIsValid) {
-    return accessToken;
-  }
-  throw new Error('Access token was not found in localStorage');
-};
-
-export const getRequestHeadersWithAccessToken = () => {
-  const headers = defaultHeaders as Headers;
-  try {
-    const accessToken = getAccessTokenFromLocalStorage();
-    headers['Authorization'] = `Bearer ${accessToken}`;
-  } catch {
-    // do nothing
-  }
-  return { headers };
 };
