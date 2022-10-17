a96601f953693ea1f1a53a02c85d4a806abcc13c
Sergei Burtsev <spburtsev@proton.me>
2022-10-18 00:37:38 +0300

    tailwind.ts file removed

--- src/styles/tailwind.ts
+++
@@ -1,2 +0,0 @@
-export const formInputClass =
-  'border-darkerGrey border border-solid rounded-lg bg-primaryGrey transition ease-in-out duration-200 focus:border-lightBlueAccent focus:outline-none';
