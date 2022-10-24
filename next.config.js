f951c4e15338238ba15ed84a3680464e7f5d08ff
Sergei Burtsev <spburtsev@proton.me>
2022-10-24 23:27:56 +0300

    next config edited

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   swcMinify: true,
   env: {
     BACKEND_SERVER_URL: process.env.BACKEND_SERVER_URL,
+    BACKEND_IMAGE_DOMAIN: process.env.BACKEND_IMAGE_DOMAIN,
   },
   images: {
     domains: [process.env.BACKEND_IMAGE_DOMAIN],
