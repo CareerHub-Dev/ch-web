2532ec51289a069b7551427ea8fae0aba45c3622
Sergei Burtsev <spburtsev@proton.me>
2023-02-12 14:08:05 +0200

    centered spinner minor adjustment

--- src/components/ui/CenteredLoadingSpinner.tsx
+++ src/components/ui/CenteredLoadingSpinner.tsx
@@ -2,7 +2,7 @@ import LoadingSpinner from '@/components/ui/LoadingSpinner';
 
 export default function CenteredLoadingSpinner() {
   return (
-    <div className="flex justify-center p-8">
+    <div className="flex justify-center p-4">
       <LoadingSpinner />
     </div>
   );
