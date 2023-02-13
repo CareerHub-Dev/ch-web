821e591003ef43e2be93ba78d08fa11a64aa5263
Sergei Burtsev <spburtsev@proton.me>
2023-02-13 23:02:03 +0200

    Inaccesible stage buttons disabled

--- src/features/cv-builder/components/layout/StageButton.tsx
+++ src/features/cv-builder/components/layout/StageButton.tsx
@@ -1,5 +1,6 @@
 import {
   getStageCompletionStatus,
+  getStageAccessibility,
   useCvDataStore,
   CV_EDITOR_STAGES,
 } from "../../store/cv-data-store";
@@ -12,10 +13,16 @@ export default function StageButton({
 }) {
   const currentStageNumber = useCvDataStore((s) => s.currentStage);
   const stageStatus = useCvDataStore(getStageCompletionStatus(stage.id));
+  const stageAccessibility = useCvDataStore(getStageAccessibility(stage.id));
   const goToStage = useCvDataStore((s) => s.goToStage);
 
-  const colorStatus = currentStageNumber === stage.id ? "current" : stageStatus;
-  const colorClass = getStageColorClass(colorStatus);
+  const colorStatus =
+    currentStageNumber === stage.id
+      ? "current"
+      : stageAccessibility.status === "inaccessible"
+      ? "disabled"
+      : stageStatus;
+  const colorClasses = getStageColorClasses(colorStatus);
 
   const handleClick = () => {
     goToStage(stage.id);
@@ -26,12 +33,19 @@ export default function StageButton({
       <button
         type="button"
         onClick={handleClick}
+        disabled={stageAccessibility.status === "inaccessible"}
+        title={stageAccessibility.reason}
         className={cn(
           "text-left group flex flex-col border-l-4 py-2 pl-4 lg:border-l-0 lg:border-t-4 lg:pl-0 lg:pt-4 lg:pb-0 w-full transition-all ease-in-out duration-200",
-          colorClass
+          colorClasses.button
         )}
       >
-        <span className="text-sm font-medium text-gray-500 group-hover:text-gray-700">
+        <span
+          className={cn(
+            "text-sm font-medium text-gray-500",
+            colorClasses.title
+          )}
+        >
           {`${stage.id + 1}. ${stage.name}`}
         </span>
       </button>
@@ -39,21 +53,36 @@ export default function StageButton({
   );
 }
 
-function getStageColorClass(
+function getStageColorClasses(
   stageStatus:
     | ReturnType<ReturnType<typeof getStageCompletionStatus>>
     | "current"
+    | "disabled"
 ) {
   switch (stageStatus) {
     case "current":
-      return "border-blue-600";
+      return { button: "border-blue-600", title: "group-hover:text-gray-700" };
     case "hasErrors":
-      return "border-red-600 hover:border-red-700";
+      return {
+        button: "border-red-600 hover:border-red-700",
+        title: "group-hover:text-gray-700",
+      };
     case "hasWarnings":
-      return "border-orange-600 hover:border-orange-700";
+      return {
+        button: "border-orange-600 hover:border-orange-700",
+        title: "group-hover:text-gray-700",
+      };
     case "complete":
-      return "border-green-600 hover:border-green-700";
+      return {
+        button: "border-green-600 hover:border-green-700",
+        title: "group-hover:text-gray-700",
+      };
+    case "disabled":
+      return { button: "border-gray-200 cursor-not-allowed" };
     default:
-      return "border-gray-200 hover:border-gray-300";
+      return {
+        button: "border-gray-200 hover:border-gray-300",
+        title: "group-hover:text-gray-700",
+      };
   }
 }
