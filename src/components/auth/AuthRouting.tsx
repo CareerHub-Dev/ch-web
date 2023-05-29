932f37b3da8ed8e80b0ca1464fbb9ccae6c21455
hyperborean_one <spburtsev@proton.me>
2023-05-29 14:08:51 +0300

    Minor auth fields fix on login page

--- src/components/auth/AuthRouting.tsx
+++ src/components/auth/AuthRouting.tsx
@@ -1,53 +1,58 @@
 import { useRouter } from "next/router";
 import classes from "./AuthRouting.module.scss";
 
-const AuthRouting = () => {
-  const router = useRouter();
-  const currentForm = router.query.form as string;
+export default function AuthRouting() {
+    const router = useRouter();
+    const currentForm = router.query.form as string;
 
-  const authFormChangeHandler = (newSection: string) => {
-    router.push(newSection);
-  };
+    const authFormChangeHandler = (newSection: string) => {
+        router.push(newSection);
+    };
 
-  return (
-    <div className={classes.controls}>
-      {currentForm !== "register" && (
-        <button
-          id="formSwitchButton-Register"
-          onClick={authFormChangeHandler.bind(null, "register")}
-          className="text-sm"
-        >
-          Зареєструватися
-        </button>
-      )}
-      {currentForm !== "login" && (
-        <button
-          id="formSwitchButton-Login"
-          onClick={authFormChangeHandler.bind(null, "login")}
-          className="text-sm"
-        >
-          Увійти
-        </button>
-      )}
-      {currentForm !== "forgot-password" && (
-        <button
-          id="formSwitchButton-forget"
-          onClick={authFormChangeHandler.bind(null, "forgot-password")}
-          className="text-sm"
-        >
-          Забув пароль?
-        </button>
-      )}
-      {currentForm !== "activate-account" ? (
-        <button
-          id="formSwitchButton-activate"
-          onClick={authFormChangeHandler.bind(null, "activate-account")}
-          className="text-sm"
-        >
-          {"Активувати акаунт"}
-        </button>
-      ) : null}
-    </div>
-  );
+    return (
+        <div className={classes.controls}>
+            {currentForm !== "register" && (
+                <button
+                    id="formSwitchButton-Register"
+                    onClick={authFormChangeHandler.bind(null, "register")}
+                    className="text-sm"
+                >
+                    Зареєструватися
+                </button>
+            )}
+            {currentForm !== "login" && (
+                <button
+                    id="formSwitchButton-Login"
+                    onClick={authFormChangeHandler.bind(null, "login")}
+                    className="text-sm"
+                >
+                    Увійти
+                </button>
+            )}
+            {currentForm !== "forgot-password" && (
+                <button
+                    id="formSwitchButton-forget"
+                    onClick={authFormChangeHandler.bind(
+                        null,
+                        "forgot-password"
+                    )}
+                    className="text-sm"
+                >
+                    Забули пароль?
+                </button>
+            )}
+            {currentForm !== "activate-account" ? (
+                <button
+                    id="formSwitchButton-activate"
+                    onClick={authFormChangeHandler.bind(
+                        null,
+                        "activate-account"
+                    )}
+                    className="text-sm"
+                >
+                    {"Активувати акаунт"}
+                </button>
+            ) : null}
+        </div>
+    );
 };
-export default AuthRouting;
